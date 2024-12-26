62bc13039f225638ae8a135ea198fea48dd5cecd
Efe Sezen <efesezen1@gmail.com>
2024-12-26 12:03:34 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+   <div class="container w-screen h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline ">Panel</h1>
+   </div>
+</template>
+
+<style scoped></style>
