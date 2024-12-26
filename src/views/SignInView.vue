e7c7121f85783faaa8c28f4e2477ffa3517c5f56
Efe Sezen <efesezen1@gmail.com>
2024-12-26 19:53:44 +0300

    UI update for error handling is added for auth.

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -6,6 +6,8 @@ const email = ref('')
 const password = ref('')
 const router = useRouter()
 const errMsg = ref()
+const emailInput = ref(null)
+const passwordInput = ref(null)
 const date = new Date()
 
 const signIn = () => {
@@ -22,15 +24,20 @@ const signIn = () => {
          switch (error.code) {
             case 'auth/invalid-email':
                errMsg.value = 'Invalid email'
+               emailInput.value.focus()
                break
             case 'auth/user-not-found':
                errMsg.value = 'User not found'
+               emailInput.value.focus()
                break
             case 'auth/wrong-password':
                errMsg.value = 'Wrong password'
+               password.value = ''
+               passwordInput.value.focus()
                break
             default:
                errMsg.value = 'Email or password was incorrect'
+               emailInput.value.focus()
                break
          }
       })
@@ -39,17 +46,22 @@ const signIn = () => {
 
 <template>
    <div class="flex-center-col">
-      <form class="container flex flex-col items-center gap-4 h-full py-10">
+      <form
+         @submit.prevent="signIn"
+         class="container flex flex-col items-center gap-4 h-full py-10"
+      >
          <img src="/icon.png" alt="" class="scale-50" />
          <div class="capitalize text-3xl">Please Sign In</div>
          <div class="signin-dimensions flex flex-col">
             <input
+               ref="emailInput"
                type="email"
                v-model="email"
                placeholder="E-mail address"
                class="rounded-t-md signin-input"
             />
             <input
+               ref="passwordInput"
                type="password"
                v-model="password"
                placeholder="Password"
@@ -59,13 +71,19 @@ const signIn = () => {
 
          <button
             type="submit"
-            class="signin-dimensions bg-gradient-to-tr from-[#4158CA] to-[#5770EE] p-3 rounded-md"
-            @click.prevent="signIn"
+            class="signin-dimensions btn-primary-color p-3 rounded-md transition-all duration-300"
          >
             Sign in
          </button>
-         {{ errMsg }}
-         <footer>
+         <div
+            class="transition-all duration-300"
+            :class="`${
+               errMsg ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+            }`"
+         >
+            {{ errMsg }}
+         </div>
+         <footer class="absolute bottom-0 right-0 p-10">
             <p class="opacity-50">Codeway &copy; {{ date.getFullYear() }}</p>
          </footer>
       </form>
